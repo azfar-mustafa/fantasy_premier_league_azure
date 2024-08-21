9e69de9c0079537174be8f45e92ffe032cb5eeb4
azfar mustafa <khairulbakar95@gmail.com>
2024-08-21 23:42:10 +0800

    Change archive file functionality

--- Archive_file/__init__.py
+++ Archive_file/__init__.py
@@ -4,88 +4,54 @@ from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
 from azure.storage.filedatalake import DataLakeServiceClient
 import azure.functions as func
-from datetime import datetime
 
 
-def list_blob(storage_account_url, container_name, credential):
-    blob_service_client = BlobServiceClient(storage_account_url, credential=credential)
-    container_client = blob_service_client.get_container_client(container_name)
-    blob_list = container_client.list_blobs()
+def copy_file_to_archive(service_client, container_name: str, data_source_type, current_date, file_name):
+    try:
+        file_system_client = service_client.get_file_system_client(container_name)
+        source_path = f"landing/{data_source_type}/current/{current_date}/{file_name}"
+        destination_path = f"landing/{data_source_type}/archive/{current_date}/{file_name}"
+        source_file = file_system_client.get_file_client(source_path)
+        dest_file = file_system_client.get_file_client(destination_path)
+        if not source_file.exists():
+            logging.info(f"There is no file in {data_source_type} to be archived")
+        else:
+            dest_file.upload_data(source_file.download_file().readall(), overwrite=True)
+            source_file.delete_file()
+            logging.info(f"File {file_name} copied to archive and deleted from current folder")
+    ## Reference - https://stackoverflow.com/questions/32500935/python-how-to-move-or-copy-azure-blob-from-one-container-to-another
+    except Exception as e:
+        logging.error(f"An error occurred while copying and deleting the file: {str(e)}")
 
-    blob_name = []
-
-    for blob in blob_list:
-        blob_name.append(blob.name)
-
-    return blob_name
-
-
-def get_old_date(list_of_files):
-    date_counts = set()
-    for path in list_of_files:
-        parts = path.split('/')
-        if len(parts) > 2 and parts[2].isdigit() and "current" in parts:
-            date_counts.add(parts[2])
-    if date_counts:
-        return min(date_counts)
-    return None
-
-
-def copy_file_to_archive(archive_date, storage_account_url, container_name, list_of_files, credential):
-    blob_service_client = BlobServiceClient(storage_account_url, credential=credential)
-    container_client = blob_service_client.get_container_client(container_name)
-
-    # Reference - https://stackoverflow.com/questions/32500935/python-how-to-move-or-copy-azure-blob-from-one-container-to-another
-    for folder_path in list_of_files:
-        folder_path_list = folder_path.split("/")
-        if len(folder_path_list) > 1:
-              if "current" in folder_path_list and archive_date in folder_path_list and len(folder_path_list) > 3:
-                source_blob_path = "/".join(folder_path_list)
-                folder_path_list[1] = "archive"
-                destination_blob_path = "/".join(folder_path_list)
-                source_blob = container_client.get_blob_client(source_blob_path)
-                destination_blob = container_client.get_blob_client(destination_blob_path)
-                destination_blob.start_copy_from_url(source_blob.url)
-                source_blob.delete_blob()
-
-
-def delete_old_directory(data_lake_url, container_name, archive_date, credential):
-    datalake_service_client = DataLakeServiceClient(account_url=data_lake_url, credential=credential)
-    file_system_client = datalake_service_client.get_file_system_client(container_name)
-    path_list = file_system_client.get_paths()
-
-    # Reference - https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/storage/azure-storage-file-datalake/samples/datalake_samples_file_system.py
-    for path in path_list:
-        path_name = path.name
-        parts = path_name.split("/")
-        print(parts)
-        if len(parts) > 2 and parts[2] == archive_date and parts[1] == "current":
-            parts_path = "/".join(parts)
-            print(parts_path)
-            file_system_to_delete = file_system_client.get_directory_client(parts_path)
-            file_system_to_delete.delete_directory()
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
-    storage_account_url = os.getenv("StorageAccountUrl")
+    data_source_type = req.params.get('data_source')
+    file_date = req.params.get('file_date')
+
+    if not data_source_type:
+        return func.HttpResponse(
+            "No parameter supplied. Please provide a 'data_source' parameter. Input could be either current_season_history or player_metadata or team_metadata or position_metadata",
+            status_code=400
+        )
+    elif not file_date:
+        return func.HttpResponse(
+            "No parameter supplied. Please provide a 'file_date' based on the file to be ingested. Date format should be ddMMyyyy",
+            status_code=400
+        )
+    
     data_lake_url = os.getenv("DataLakeUrl")
     storage_account_container = os.getenv("StorageAccountContainer")
     default_credential = DefaultAzureCredential()
+    service_client = DataLakeServiceClient(account_url=data_lake_url, credential=default_credential)
+
+    file_name = f'{data_source_type}_{file_date}.json'
 
     try:
-        blob_list = list_blob(storage_account_url, storage_account_container, default_credential)
-        oldest_date = get_old_date(blob_list)
-        
-        if oldest_date:
-            copy_file_to_archive(oldest_date, storage_account_url, storage_account_container, blob_list, default_credential)
-            delete_old_directory(data_lake_url, storage_account_container, oldest_date, default_credential)
-            logging.info(f"File date {oldest_date} is archived.")
-            return func.HttpResponse(f"File date {oldest_date} is archived.", status_code=200)
-        else:
-            logging.info("No need to archive file")
-            return func.HttpResponse(f"No need to archive file.", status_code=200)
+        copy_file_to_archive(service_client, storage_account_container, data_source_type, file_date, file_name)
+        return func.HttpResponse(f"Process Completed.", status_code=200)
     except Exception as e:
         logging.error(f"An error occured: {str(e)}")
         return func.HttpResponse(f"An error occured: {str(e)}", status_code=500)
